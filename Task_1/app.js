c9876abec32da38ec03c1e6efbe6a4246f27325d
Bazi-Alina1 <74421027+Bazi-Alina1@users.noreply.github.com>
2022-01-21 14:20:54 +0200

    Am modificat Task 1,2

--- Task_1/app.js
+++ Task_1/app.js
@@ -1,11 +1,25 @@
-alert("Salut. Eu sunt JavaScript");
-
-const userName = prompt("Cum te numesti?");
-
-alert(`Salut ${userName}`);
-
-if(confirm("Vreti sa studiati JS?")){
-    alert("Bravo! Succes!")
-} else{
-    alert("OOps. Ati ales directia gresita");
+let n = '9';
+switch(n){
+    case '0': console.log('zero');
+    break;
+ case '1': console.log('unu');
+    break;
+    case '2': console.log('doi');
+    break;
+    case '3': console.log('trei');
+    break;
+    case '4': console.log('patru');
+    break;
+    case '5': console.log('cinci');
+    break;
+    case '6': console.log('sase');
+    break;
+    case '7': console.log('sapte');
+    break;
+    case '8': console.log('opt');
+    break;
+    case '9': console.log('noua');
+    break;
+    default: console.log('Ati introdus nr.gresit!')
+    
 }
---
+++ Task_2/app.js
@@ -0,0 +1,12 @@
+let a =1;
+let b=10;
+let suma=0;
+for (i=a; i<= b; i++) 
+{
+    if(i %2 == 0){
+        suma = suma + i;
+        
+    }
+    
+} 
+console.log('Suma:', suma)
