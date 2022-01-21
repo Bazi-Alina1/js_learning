c9c4022889d9342c6020b2d661ecd9d84083de71
Bazi-Alina1 <74421027+Bazi-Alina1@users.noreply.github.com>
2022-01-21 14:36:01 +0200

    Am adaugat Task 3

---
+++ Task_3/app.js
@@ -0,0 +1,13 @@
+let a=4;
+let b= 6;
+let c=2;
+if  (a < b){
+    if (b > c){
+        console.log('Cel mai mare' , b)
+    }else{console.log('Cel mai mare' , c) }
+}
+else{
+    if (a > c){
+        console.log('Cel mai mare' , a)
+    }else{console.log('Cel mai mare' , c) }
+}
